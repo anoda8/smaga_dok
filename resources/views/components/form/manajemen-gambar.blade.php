6aef7bb10bd20bfb3461582c72b4bfa790b1d5fd
anoda8 <nosada29@gmail.com>
2024-01-05 15:18:26 +0700

    manajemen gambar

--- resources/views/components/form/manajemen-gambar.blade.php
+++ resources/views/components/form/manajemen-gambar.blade.php
@@ -1,6 +1,27 @@
 <div>
     {{-- Because she competes with no one, no one can compete with her. --}}
-    Manajemen Gambar {{ $activityId }}
+
+    <div class="card">
+        <div class="card-header">
+            Manajemen Gambar {{ $activityId }}
+        </div>
+        <div class="card-body">
+            <div class="row mt-3">
+                <div class="col-sm-12 col-md-3">
+                    <img src="https://dummyimage.com/600x400/000/fff" class="img-thumbnail">
+                </div>
+                <div class="col-sm-12 col-md-3">
+                    <img src="https://dummyimage.com/600x400/000/fff" class="img-thumbnail">
+                </div>
+                <div class="col-sm-12 col-md-3">
+                    <img src="https://dummyimage.com/600x400/000/fff" class="img-thumbnail">
+                </div>
+                <div class="col-sm-12 col-md-3">
+                    <img src="https://dummyimage.com/600x400/000/fff" class="img-thumbnail">
+                </div>
+            </div>
+        </div>
+    </div>
 
 </div>
 
